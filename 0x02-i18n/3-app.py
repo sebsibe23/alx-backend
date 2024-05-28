b33ee6789b48622cd4e45be1503b8c3d5b2d1c1b
sebsibe23 <sebsibe1990@proton.me>
2024-05-28 12:04:10 -0700

    task 3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -8,13 +8,14 @@ from flask import Flask, render_template, request
 
 class Config:
     """
-    Represents a Flask Babel configuration.
+     Represents a Flask Babel configuration.
 
     Attributes:
     - LANGUAGES (list): Supported languages.
     - BABEL_DEFAULT_LOCALE (str): Default locale.
     - BABEL_DEFAULT_TIMEZONE (str): Default timezone.
     """
+
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -29,35 +30,19 @@ babel = Babel(app)
 @babel.localeselector
 def get_locale() -> str:
     """
-    Function: get_locale
-    Description: Retrieves the best match for the locale from the
-    request's Accept-Languages header.
-    Returns:
-    - str: Best match locale from supported languages.
-    """
-    try:
-        return request.accept_languages.best_match(app.config["LANGUAGES"])
-    except Exception as e:
-        # Handle exceptions and log error message
-        app.logger.error(f"An error occurred while selecting locale: {e}")
-        return app.config["BABEL_DEFAULT_LOCALE"]
-
-
-@app.route('/')
-def get_index() -> str:
-    """
-    Function: get_index
+     Function: get_index
     Description: The home/index page route.
     Returns:
     - str: Rendered HTML template for the home page.
     """
-    try:
-        return render_template('3-index.html')
-    except Exception as e:
-        # Handle exceptions and log error message
-        app.logger.error(f"An error occurred while rendering the page: {e}")
-        return "An error occurred while rendering the page."
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+@app.route("/")
+def get_index() -> str:
+    """The home/index page."""
+    return render_template("3-index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
